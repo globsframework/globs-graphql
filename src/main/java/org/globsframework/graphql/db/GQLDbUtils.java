b8d910990397b79ce9d4d819004330869081a4ee
MarcGuiot <marc.guiot@free.fr>
2023-02-22 18:36:19 +0100

    Take key from a cache.

--- src/main/java/org/globsframework/graphql/db/GQLDbUtils.java
+++ src/main/java/org/globsframework/graphql/db/GQLDbUtils.java
@@ -1,7 +1,9 @@
 package org.globsframework.graphql.db;
 
+import org.globsframework.functional.FunctionalKey;
 import org.globsframework.graphql.OnKey;
 import org.globsframework.metamodel.fields.StringField;
+import org.globsframework.model.Glob;
 import org.globsframework.sqlstreams.SelectQuery;
 import org.globsframework.sqlstreams.SqlConnection;
 import org.globsframework.sqlstreams.constraints.Constraint;
@@ -11,10 +13,37 @@ import org.globsframework.utils.collections.MultiMap;
 import java.util.List;
 
 public class GQLDbUtils {
-    public static void queryByKey(SqlConnection db, StringField fKeyField, StringField dbKeyField, List<OnKey> parents, Constraint additionalConstraint) {
+    public interface Cached {
+        Cached NULL = new Cached() {
+            public Glob get(FunctionalKey functionalKey) {
+                return null;
+            }
+
+            public void push(FunctionalKey functionalKey, Glob glob) {
+
+            }
+        };
+        Glob get(FunctionalKey functionalKey);
+
+        void push(FunctionalKey functionalKey, Glob glob);
+    }
+
+    public static void queryByKey(SqlConnection db, StringField fKeyField, StringField dbKeyField, List<OnKey> parents,
+                                  Constraint additionalConstraint) {
+        queryByKey(db, fKeyField, dbKeyField, parents, additionalConstraint, Cached.NULL);
+    }
+
+    public static void queryByKey(SqlConnection db, StringField fKeyField, StringField dbKeyField,
+                                  List<OnKey> parents, Constraint additionalConstraint, Cached cached) {
         MultiMap<String, OnKey> keyOnLoadMap = new MultiMap<>();
         for (OnKey parent : parents) {
-            keyOnLoadMap.put(parent.key().get(fKeyField), parent);
+            final Glob glob = cached.get(parent.key());
+            if (glob != null) {
+                parent.onNew().push(glob);
+            }
+            else {
+                keyOnLoadMap.put(parent.key().get(fKeyField), parent);
+            }
         }
         final Constraint in = Constraints.in(dbKeyField, keyOnLoadMap.keySet());
         final SelectQuery query = db.getQueryBuilder(dbKeyField.getGlobType(), Constraints.and(additionalConstraint, in))
@@ -24,6 +53,7 @@ public class GQLDbUtils {
             final String s = glob.get(dbKeyField);
             final List<OnKey> onKeys = keyOnLoadMap.get(s);
             for (OnKey onKey : onKeys) {
+                cached.push(onKey.key(), glob);
                 onKey.onNew().push(glob);
             }
         });
